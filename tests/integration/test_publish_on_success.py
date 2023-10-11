fb8d4910f2e75671cb2714f483ff5e9a80ae1392
Mohammad Amin <dadgaramin96@gmail.com>
2023-10-11 17:25:41 +0330

    fix: updated docker-compose with new env variable and one test updated!

--- tests/integration/test_publish_on_success.py
+++ tests/integration/test_publish_on_success.py
@@ -1,6 +1,12 @@
 import os
 from datetime import datetime, timedelta
 
+from automation.utils.interfaces import (
+    Automation,
+    AutomationAction,
+    AutomationReport,
+    AutomationTrigger,
+)
 from discord_utils import publish_on_success
 from dotenv import load_dotenv
 from utils.daolytics_uitls import get_mongo_credentials
@@ -15,18 +21,25 @@ def test_publish_on_success_check_notification_choreographies():
     """
     load_dotenv()
 
-    guildId = "915914985140531240"
+    guild_id = "1234"
     saga_id = "000000011111113333377777ie0w"
     expected_owner_id = "334461287892"
-    db_access = launch_db_access(guildId)
+    db_access = launch_db_access(guild_id)
     saga_db = os.getenv("SAGA_DB_NAME")
     saga_collection = os.getenv("SAGA_DB_COLLECTION")
+    at_db = os.getenv("AUTOMATION_DB_NAME")
+    at_collection = os.getenv("AUTOMATION_DB_COLLECTION")
 
     db_access.db_mongo_client["RnDAO"].drop_collection("guilds")
 
+    db_access.db_mongo_client[guild_id].drop_collection("memberactivities")
+    db_access.db_mongo_client[saga_db].drop_collection(saga_collection)
+    db_access.db_mongo_client[guild_id].drop_collection("guildmembers")
+    db_access.db_mongo_client[at_db].drop_collection(at_collection)
+
     db_access.db_mongo_client["RnDAO"]["guilds"].insert_one(
         {
-            "guildId": guildId,
+            "guildId": guild_id,
             "user": expected_owner_id,
             "name": "Sample Guild",
             "connectedAt": datetime.now(),
@@ -44,43 +57,6 @@ def test_publish_on_success_check_notification_choreographies():
         }
     )
 
-    db_access.db_mongo_client[guildId].drop_collection("guildmembers")
-    db_access.db_mongo_client[guildId]["guildmembers"].delete_many({})
-
-    db_access.db_mongo_client[guildId]["guildmembers"].insert_many(
-        [
-            {
-                "discordId": "1111",
-                "username": "user1",
-                "roles": [],
-                "joinedAt": datetime.now() - timedelta(days=10),
-                "avatar": None,
-                "isBot": False,
-                "discriminator": "0",
-                "permissions": "6677",
-                "deletedAt": None,
-                "globalName": "User1GlobalName",
-                "nickname": "User1NickName",
-            },
-            {
-                "discordId": "1112",
-                "username": "user2",
-                "roles": [],
-                "joinedAt": datetime.now() - timedelta(days=10),
-                "avatar": None,
-                "isBot": False,
-                "discriminator": "0",
-                "permissions": "6677",
-                "deletedAt": None,
-                "globalName": "User2GlobalName",
-                "nickname": None,
-            },
-        ]
-    )
-
-    db_access.db_mongo_client[guildId].drop_collection("memberactivities")
-    db_access.db_mongo_client[guildId]["memberactivities"].delete_many({})
-
     # Adding sample memberactivities
     date_yesterday = (
         (datetime.now() - timedelta(days=1))
@@ -93,60 +69,6 @@ def test_publish_on_success_check_notification_choreographies():
         .replace(hour=0, minute=0, second=0)
         .strftime("%Y-%m-%dT%H:%M:%S")
     )
-
-    db_access.db_mongo_client[guildId]["memberactivities"].insert_many(
-        [
-            {
-                "date": date_yesterday,
-                "all_joined": [],
-                "all_joined_day": [],
-                "all_consistent": [],
-                "all_vital": [],
-                "all_active": [],
-                "all_connected": [],
-                "all_paused": [],
-                "all_new_disengaged": ["1111", "1112"],
-                "all_disengaged": [],
-                "all_unpaused": [],
-                "all_returned": [],
-                "all_new_active": [],
-                "all_still_active": [],
-                "all_dropped": [],
-                "all_disengaged_were_newly_active": [],
-                "all_disengaged_were_consistently_active": [],
-                "all_disengaged_were_vital": [],
-                "all_lurker": [],
-                "all_about_to_disengage": [],
-                "all_disengaged_in_past": [],
-            },
-            {
-                "date": date_two_past_days,
-                "all_joined": [],
-                "all_joined_day": [],
-                "all_consistent": [],
-                "all_vital": [],
-                "all_active": [],
-                "all_connected": [],
-                "all_paused": [],
-                "all_new_disengaged": ["user3", "user6", "user9"],
-                "all_disengaged": [],
-                "all_unpaused": [],
-                "all_returned": [],
-                "all_new_active": [],
-                "all_still_active": [],
-                "all_dropped": [],
-                "all_disengaged_were_newly_active": [],
-                "all_disengaged_were_consistently_active": [],
-                "all_disengaged_were_vital": [],
-                "all_lurker": [],
-                "all_about_to_disengage": [],
-                "all_disengaged_in_past": [],
-            },
-        ]
-    )
-
-    # Adding a sample saga
-    db_access.db_mongo_client[saga_db].drop_collection(saga_collection)
 
     db_access.db_mongo_client[saga_db][saga_collection].insert_one(
         {
@@ -175,7 +97,7 @@ def test_publish_on_success_check_notification_choreographies():
             },
             "status": "IN_PROGRESS",
             "data": {
-                "guildId": guildId,
+                "guildId": guild_id,
                 "created": False,
                 "discordId": expected_owner_id,
                 "message": "data is ready",
@@ -185,6 +107,189 @@ def test_publish_on_success_check_notification_choreographies():
             "createdAt": datetime.now(),
             "updatedAt": datetime.now(),
         }
+    )
+
+    db_access.db_mongo_client[guild_id]["guildmembers"].insert_many(
+        [
+            {
+                "discordId": "1111",
+                "username": "user1",
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": "User1GlobalName",
+                "nickname": "User1NickName",  # this will be used for the message
+            },
+            {
+                "discordId": "1112",
+                "username": "user2",
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": "User2GlobalName",  # this will be used for the message
+                "nickname": None,
+            },
+            {
+                "discordId": "1113",
+                "username": "user3",  # this will be used for the message
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": None,
+                "nickname": None,
+            },
+            {
+                "discordId": "1116",
+                "username": "user6",
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": "User6GlobalName",
+                "nickname": "User6NickName",
+            },
+            {
+                "discordId": "1119",
+                "username": "user9",
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": "User9GlobalName",
+                "nickname": None,
+            },
+            {
+                "discordId": "999",
+                "username": "community_manager",
+                "roles": [],
+                "joinedAt": datetime.now() - timedelta(days=10),
+                "avatar": None,
+                "isBot": False,
+                "discriminator": "0",
+                "permissions": "6677",
+                "deletedAt": None,
+                "globalName": "User9GlobalName",
+                "nickname": None,
+            },
+        ]
+    )
+
+    triggers = [
+        AutomationTrigger(options={"category": "all_new_disengaged"}, enabled=True),
+        AutomationTrigger(options={"category": "all_new_active"}, enabled=False),
+    ]
+    actions = [
+        AutomationAction(
+            template="hey {{ngu}}! please get back to us!",
+            options={},
+            enabled=True,
+        ),
+        AutomationAction(
+            template="hey {{ngu}}! please get back to us2!",
+            options={},
+            enabled=False,
+        ),
+    ]
+
+    report = AutomationReport(
+        recipientIds=["999"],
+        template="hey body! This users were messaged:\n{{#each usernames}}{{this}}{{/each}}",
+        options={},
+        enabled=True,
+    )
+    today_time = datetime.now()
+
+    automation = Automation(
+        guild_id,
+        triggers,
+        actions,
+        report,
+        enabled=True,
+        createdAt=today_time,
+        updatedAt=today_time,
+    )
+
+    db_access.db_mongo_client[at_db][at_collection].insert_one(automation.to_dict())
+
+    date_yesterday = (
+        (datetime.now() - timedelta(days=1))
+        .replace(hour=0, minute=0, second=0)
+        .strftime("%Y-%m-%dT%H:%M:%S")
+    )
+
+    date_two_past_days = (
+        (datetime.now() - timedelta(days=2))
+        .replace(hour=0, minute=0, second=0)
+        .strftime("%Y-%m-%dT%H:%M:%S")
+    )
+
+    db_access.db_mongo_client[guild_id]["memberactivities"].insert_many(
+        [
+            {
+                "date": date_yesterday,
+                "all_joined": [],
+                "all_joined_day": [],
+                "all_consistent": [],
+                "all_vital": [],
+                "all_active": [],
+                "all_connected": [],
+                "all_paused": [],
+                "all_new_disengaged": ["1111", "1112", "1113"],
+                "all_disengaged": [],
+                "all_unpaused": [],
+                "all_returned": [],
+                "all_new_active": [],
+                "all_still_active": [],
+                "all_dropped": [],
+                "all_disengaged_were_newly_active": [],
+                "all_disengaged_were_consistently_active": [],
+                "all_disengaged_were_vital": [],
+                "all_lurker": [],
+                "all_about_to_disengage": [],
+                "all_disengaged_in_past": [],
+            },
+            {
+                "date": date_two_past_days,
+                "all_joined": [],
+                "all_joined_day": [],
+                "all_consistent": [],
+                "all_vital": [],
+                "all_active": [],
+                "all_connected": [],
+                "all_paused": [],
+                "all_new_disengaged": ["1116", "1119"],
+                "all_disengaged": [],
+                "all_unpaused": [],
+                "all_returned": [],
+                "all_new_active": [],
+                "all_still_active": [],
+                "all_dropped": [],
+                "all_disengaged_were_newly_active": [],
+                "all_disengaged_were_consistently_active": [],
+                "all_disengaged_were_vital": [],
+                "all_lurker": [],
+                "all_about_to_disengage": [],
+                "all_disengaged_in_past": [],
+            },
+        ]
     )
 
     # preparing the data for publish_on_success function
@@ -203,21 +308,32 @@ def test_publish_on_success_check_notification_choreographies():
     sample_args_data = ["sample", saga_id, mongo_creds]
     publish_on_success(None, None, sample_args_data)
 
-    cursor = db_access.db_mongo_client[saga_db][saga_collection].find(
-        {"choreography.name": "DISCORD_NOTIFY_USERS"}
+    notification_count = db_access.db_mongo_client[saga_db][
+        saga_collection
+    ].count_documents({"choreography.name": "DISCORD_NOTIFY_USERS"})
+
+    assert notification_count == 4
+
+    user1_doc = db_access.db_mongo_client[saga_db][saga_collection].find_one(
+        {"data.discordId": "1111"}
+    )
+    assert user1_doc["data"]["message"] == ("hey User1NickName! please get back to us!")
+
+    user2_doc = db_access.db_mongo_client[saga_db][saga_collection].find_one(
+        {"data.discordId": "1112"}
+    )
+    assert user2_doc["data"]["message"] == (
+        "hey User2GlobalName! please get back to us!"
     )
 
-    notification_sagas = list(cursor)
-    # two notified users
-    # and one notified guild owner
-    assert len(notification_sagas) == 3
+    user3_doc = db_access.db_mongo_client[saga_db][saga_collection].find_one(
+        {"data.discordId": "1113"}
+    )
+    assert user3_doc["data"]["message"] == ("hey user3! please get back to us!")
 
-    for saga in notification_sagas:
-        assert saga["data"]["discordId"] in ["1111", "1112", expected_owner_id]
-        # the owner of the guild receives a different message
-        if saga["data"]["discordId"] == expected_owner_id:
-            expected_message = "The following members disengaged and were messaged:\n"
-            expected_message += "- User1NickName\n"
-            expected_message += "- User2GlobalName\n"
-
-            saga["data"]["message"] == expected_message
+    user_cm_doc = db_access.db_mongo_client[saga_db][saga_collection].find_one(
+        {"data.discordId": "999"}
+    )
+    expected_msg = "hey body! This users were messaged:\n"
+    expected_msg += "- User1NickName\n- User2GlobalName\n- user3\n"
+    assert user_cm_doc["data"]["message"] == expected_msg
