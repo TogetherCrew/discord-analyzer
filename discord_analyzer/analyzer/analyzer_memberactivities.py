a16e55686777512e38f680b0e5e96a00ebddb594
Mohammad Amin <dadgaramin96@gmail.com>
2023-11-07 15:10:27 +0330

    fix: Fixed the missing output for memberactivity! memberactiviites was not producing output in case of having some previous data and saving end_dates. The problem was based on saving end dates for memberactiviites documents and having the period overlapping.

--- discord_analyzer/analyzer/analyzer_memberactivities.py
+++ discord_analyzer/analyzer/analyzer_memberactivities.py
@@ -109,8 +109,10 @@ class Member_activities:
             ) * window[0]
             date_range[0] = date_range[1] - timedelta(days=num_days_to_load)
 
-        if date_range[0] < period:
-            date_range[0] = period + timedelta(days=window[0])
+            # if the date range goes back more than the "7 days `period` forward"
+            # if date_range[0] < period:
+            if date_range[0] < period + timedelta(days=window[0]):
+                date_range[0] = period + timedelta(days=window[0])
 
         # get all users during date_range
         all_users = self.utils.get_all_users(guildId)
