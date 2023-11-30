3e9e944049011001946c68647eab15fa5e87571a
Mohammad Amin <dadgaramin96@gmail.com>
2023-11-30 10:58:58 +0330

    fix: update action param of tests!

--- tests/integration/utils/activity_params.py
+++ tests/integration/utils/activity_params.py
@@ -1,32 +1,18 @@
 def prepare_activity_params():
-    INT_THR = 1  # minimum number of interactions to be active
-    UW_DEG_THR = 1  # minimum number of accounts interacted with to be active
-    PAUSED_T_THR = 1  # time period to remain paused
-    CON_T_THR = 4  # time period to be consistent active
-    CON_O_THR = 3  # time period to be consistent active
-    EDGE_STR_THR = 5  # minimum number of interactions for connected
-    UW_THR_DEG_THR = 5  # minimum number of accounts for connected
-    VITAL_T_THR = 4  # time period to assess for vital
-    VITAL_O_THR = 3  # times to be connected within VITAL_T_THR to be vital
-    STILL_T_THR = 2  # time period to assess for still active
-    STILL_O_THR = 2  # times to be active within STILL_T_THR to be still active
-    DROP_H_THR = 2
-    DROP_I_THR = 1
-
-    act_param = [
-        INT_THR,
-        UW_DEG_THR,
-        PAUSED_T_THR,
-        CON_T_THR,
-        CON_O_THR,
-        EDGE_STR_THR,
-        UW_THR_DEG_THR,
-        VITAL_T_THR,
-        VITAL_O_THR,
-        STILL_T_THR,
-        STILL_O_THR,
-        DROP_H_THR,
-        DROP_I_THR,
-    ]
+    act_param = {
+        "INT_THR": 1,
+        "UW_DEG_THR": 1,
+        "PAUSED_T_THR": 1,
+        "CON_T_THR": 4,
+        "CON_O_THR": 3,
+        "EDGE_STR_THR": 5,
+        "UW_THR_DEG_THR": 5,
+        "VITAL_T_THR": 4,
+        "VITAL_O_THR": 3,
+        "STILL_T_THR": 2,
+        "STILL_O_THR": 2,
+        "DROP_H_THR": 2,
+        "DROP_I_THR": 1,
+    }
 
     return act_param
