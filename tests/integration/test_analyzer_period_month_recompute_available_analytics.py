746315206fe706109b4deddd07f37f1b0807122e
Mohammad Amin <dadgaramin96@gmail.com>
2024-06-20 18:05:10 +0330

    fix: more test cases being updated to latest rawdata structure!

--- tests/integration/test_analyzer_period_month_recompute_available_analytics.py
+++ tests/integration/test_analyzer_period_month_recompute_available_analytics.py
@@ -15,9 +15,8 @@ def test_analyzer_month_period_recompute_available_analytics():
     and use recompute method with empty analytics available
     """
     # first create the collections
-    guildId = "1234"
     platform_id = "515151515151515151515151"
-    db_access = launch_db_access(guildId)
+    db_access = launch_db_access(platform_id)
 
     acc_id = [
         "973993299281076285",
@@ -42,8 +41,11 @@ def test_analyzer_month_period_recompute_available_analytics():
     # filling heatmaps with some data
     # filling up to 2 days ago with 29 documents
     # just yesterday is left to be analyzed
+    start_day = (
+        datetime.now() - timedelta(days=30)
+    ).replace(hour=0, minute=0, second=0, microsecond=0)
     heatmaps_data = create_empty_heatmaps_data(
-        datetime.now() - timedelta(days=30), count=29
+        start_day, count=29
     )
     db_access.db_mongo_client[platform_id]["heatmaps"].insert_many(heatmaps_data)
 
@@ -53,23 +55,39 @@ def test_analyzer_month_period_recompute_available_analytics():
     # generating random rawinfo data
     # 24 * 30 = 720
     for i in range(720):
-        sample = {
-            "type": 19,
-            "author": np.random.choice(acc_id),
-            "content": f"test{i}",
-            "user_mentions": [],
-            "role_mentions": [],
-            "reactions": [],
-            "replied_user": np.random.choice(acc_id),
-            "createdDate": (datetime.now() - timedelta(hours=i)),
-            "messageId": f"11188143219343360{i}",
-            "channelId": "1020707129214111827",
-            "channelName": "general",
-            "threadId": None,
-            "threadName": None,
-            "isGeneratedByWebhook": False,
-        }
-        rawinfo_samples.append(sample)
+        author = np.random.choice(acc_id)
+        replied_user = np.random.choice(acc_id)
+        samples = [
+            {
+                "actions": [{"name": "message", "type": "emitter"}],
+                "author_id": author,
+                "date": datetime.now() - timedelta(hours=i),
+                "interactions": [
+                    {"name": "reply", "type": "emitter", "users_engaged_id": [replied_user]}
+                ],
+                "metadata": {
+                    "bot_activity": False,
+                    "channel_id": "1020707129214111827",
+                    "thread_id": None,
+                },
+                "source_id": f"11188143219343360{i}",
+            },
+            {
+                "actions": [],
+                "author_id": replied_user,
+                "date": datetime.now() - timedelta(hours=i),
+                "interactions": [
+                    {"name": "reply", "type": "receiver", "users_engaged_id": [author]}
+                ],
+                "metadata": {
+                    "bot_activity": False,
+                    "channel_id": "1020707129214111827",
+                    "thread_id": None,
+                },
+                "source_id": f"11188143219343360{i}",
+            }
+        ]
+        rawinfo_samples.extend(samples)
 
     db_access.db_mongo_client[platform_id]["rawmemberactivities"].insert_many(
         rawinfo_samples
@@ -88,11 +106,11 @@ def test_analyzer_month_period_recompute_available_analytics():
     )
 
     assert len(memberactivities_data) == 24
-    assert memberactivities_data[0]["date"] == yesterday.isoformat()
+    assert memberactivities_data[0]["date"] == yesterday
     # yesterday is `-1` day and so
     # we would use 23 days ago rather than 24
     document_start_date = yesterday - timedelta(days=23)
-    assert memberactivities_data[-1]["date"] == (document_start_date).isoformat()
+    assert memberactivities_data[-1]["date"] == document_start_date
 
     heatmaps_cursor = db_access.query_db_find("heatmaps", {}, sorting=("date", -1))
     heatmaps_data = list(heatmaps_cursor)
@@ -101,6 +119,6 @@ def test_analyzer_month_period_recompute_available_analytics():
     # (accounts are: "973993299281076285", "973993299281076286")
     assert len(heatmaps_data) == 60
     # checking first and last document
-    assert heatmaps_data[0]["date"] == yesterday.strftime("%Y-%m-%d")
+    assert heatmaps_data[0]["date"] == yesterday
     month_ago = yesterday - timedelta(29)
-    assert heatmaps_data[-1]["date"] == month_ago.strftime("%Y-%m-%d")
+    assert heatmaps_data[-1]["date"] == month_ago
