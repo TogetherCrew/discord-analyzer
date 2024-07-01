effc3f1096f00d44547fcee7dfb2469297abdd55
Mohammad Amin <dadgaramin96@gmail.com>
2024-07-01 11:13:17 +0330

    running all test cases on github actions again!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# python3 -m coverage run --omit=tests/* -m pytest . && echo "Tests Passed" || exit 1
-python3 -m coverage run --omit=tests/* -m pytest tests/integration/test_publish_on_success.py && echo "Tests Passed" || exit 1
+python3 -m coverage run --omit=tests/* -m pytest . && echo "Tests Passed" || exit 1
 python3 -m coverage lcov -i -o coverage/lcov.info
