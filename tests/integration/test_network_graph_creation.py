f2430a5e75a051bd562c221bb585d93237a4e413
Mohammad Amin <dadgaramin96@gmail.com>
2024-07-02 17:46:36 +0330

    fix: the test case is for mock graph genertion part! It is not needed.

--- tests/integration/test_network_graph_creation.py
+++ tests/integration/test_network_graph_creation.py
@@ -1,165 +1,165 @@
 # test out local clustering coefficient with all nodes connected
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
-import networkx as nx
-import numpy as np
-import pytest
-from tc_analyzer_lib.algorithms.utils.activity import Activity
-from tc_neo4j_lib.neo4j_ops import Neo4jOps
+# import networkx as nx
+# import numpy as np
+# import pytest
+# from tc_analyzer_lib.algorithms.utils.activity import Activity
+# from tc_neo4j_lib.neo4j_ops import Neo4jOps
 
 
-@pytest.mark.skip("To be updated to platform centric!")
-def test_network_graph_create():
-    """
-    testing the mock graph generation
-    """
-    from .utils.mock_graph import generate_mock_graph, store_mock_data_in_neo4j
+# @pytest.mark.skip("To be updated to platform centric!")
+# def test_network_graph_create():
+#     """
+#     testing the mock graph generation
+#     """
+#     from .utils.mock_graph import generate_mock_graph, store_mock_data_in_neo4j
 
-    community_id = "4321"
-    neo4j_ops = Neo4jOps.get_instance()
-    # deleting all data
-    neo4j_ops.gds.run_cypher("MATCH (n) DETACH DELETE (n)")
+#     community_id = "4321"
+#     neo4j_ops = Neo4jOps.get_instance()
+#     # deleting all data
+#     neo4j_ops.gds.run_cypher("MATCH (n) DETACH DELETE (n)")
 
-    guildId = "1234"
-    acc_names = np.array(["1000", "1001", "1002"])
-    graph_dict = {}
+#     guildId = "1234"
+#     acc_names = np.array(["1000", "1001", "1002"])
+#     graph_dict = {}
 
-    # saving the desired outputs
-    desired_outputs = []
+#     # saving the desired outputs
+#     desired_outputs = []
 
-    # Generating 1st graph
-    np.random.seed(123)
-    int_matrix = {}
-    int_matrix[Activity.Reply] = np.array(
-        [
-            [0, 1, 2],
-            [0, 0, 3],
-            [0, 4, 0],
-        ]
-    )
+#     # Generating 1st graph
+#     np.random.seed(123)
+#     int_matrix = {}
+#     int_matrix[Activity.Reply] = np.array(
+#         [
+#             [0, 1, 2],
+#             [0, 0, 3],
+#             [0, 4, 0],
+#         ]
+#     )
 
-    int_matrix[Activity.Mention] = np.array(
-        [
-            [0, 1, 2],
-            [0, 0, 3],
-            [0, 4, 0],
-        ]
-    )
+#     int_matrix[Activity.Mention] = np.array(
+#         [
+#             [0, 1, 2],
+#             [0, 0, 3],
+#             [0, 4, 0],
+#         ]
+#     )
 
-    int_matrix[Activity.Reaction] = np.array(
-        [
-            [0, 1, 2],
-            [0, 0, 3],
-            [0, 4, 0],
-        ]
-    )
+#     int_matrix[Activity.Reaction] = np.array(
+#         [
+#             [0, 1, 2],
+#             [0, 0, 3],
+#             [0, 4, 0],
+#         ]
+#     )
 
-    graph = generate_mock_graph(int_matrix, acc_names)
+#     graph = generate_mock_graph(int_matrix, acc_names)
 
-    node_att = {}
-    for i, node in enumerate(list(graph)):
-        node_att[node] = acc_names[i]
+#     node_att = {}
+#     for i, node in enumerate(list(graph)):
+#         node_att[node] = acc_names[i]
 
-    nx.set_node_attributes(graph, node_att, "acc_name")
+#     nx.set_node_attributes(graph, node_att, "acc_name")
 
-    graph_date = datetime.now()
-    graph_date_timestamp = graph_date.replace(
-        hour=0, minute=0, second=0, microsecond=0
-    ).timestamp()
-    graph_dict[graph_date] = graph
+#     graph_date = datetime.now()
+#     graph_date_timestamp = graph_date.replace(
+#         hour=0, minute=0, second=0, microsecond=0
+#     ).timestamp()
+#     graph_dict[graph_date] = graph
 
-    desired_outputs.extend(
-        [
-            ["1000", 1, graph_date_timestamp, "1001"],
-            ["1000", 2, graph_date_timestamp, "1002"],
-            ["1001", 3, graph_date_timestamp, "1002"],
-            ["1002", 4, graph_date_timestamp, "1001"],
-        ]
-    )
+#     desired_outputs.extend(
+#         [
+#             ["1000", 1, graph_date_timestamp, "1001"],
+#             ["1000", 2, graph_date_timestamp, "1002"],
+#             ["1001", 3, graph_date_timestamp, "1002"],
+#             ["1002", 4, graph_date_timestamp, "1001"],
+#         ]
+#     )
 
-    # Generating 2nd graph
-    int_matrix = {}
-    int_matrix[Activity.Reply] = np.array(
-        [
-            [0, 0, 1],
-            [2, 0, 5],
-            [0, 0, 0],
-        ]
-    )
+#     # Generating 2nd graph
+#     int_matrix = {}
+#     int_matrix[Activity.Reply] = np.array(
+#         [
+#             [0, 0, 1],
+#             [2, 0, 5],
+#             [0, 0, 0],
+#         ]
+#     )
 
-    int_matrix[Activity.Mention] = np.array(
-        [
-            [0, 0, 1],
-            [2, 0, 5],
-            [0, 0, 0],
-        ]
-    )
+#     int_matrix[Activity.Mention] = np.array(
+#         [
+#             [0, 0, 1],
+#             [2, 0, 5],
+#             [0, 0, 0],
+#         ]
+#     )
 
-    int_matrix[Activity.Reaction] = np.array(
-        [
-            [0, 0, 1],
-            [2, 0, 5],
-            [0, 0, 0],
-        ]
-    )
+#     int_matrix[Activity.Reaction] = np.array(
+#         [
+#             [0, 0, 1],
+#             [2, 0, 5],
+#             [0, 0, 0],
+#         ]
+#     )
 
-    graph = generate_mock_graph(int_matrix, acc_names)
+#     graph = generate_mock_graph(int_matrix, acc_names)
 
-    nx.set_node_attributes(graph, node_att, "acc_name")
+#     nx.set_node_attributes(graph, node_att, "acc_name")
 
-    graph_date = datetime.now() + timedelta(days=-1)
-    graph_date_timestamp = graph_date.replace(
-        hour=0, minute=0, second=0, microsecond=0
-    ).timestamp()
-    graph_dict[graph_date] = graph
+#     graph_date = datetime.now() + timedelta(days=-1)
+#     graph_date_timestamp = graph_date.replace(
+#         hour=0, minute=0, second=0, microsecond=0
+#     ).timestamp()
+#     graph_dict[graph_date] = graph
 
-    desired_outputs.extend(
-        [
-            ["1000", 1, graph_date_timestamp, "1002"],
-            ["1001", 2, graph_date_timestamp, "1000"],
-            ["1001", 5, graph_date_timestamp, "1002"],
-        ]
-    )
+#     desired_outputs.extend(
+#         [
+#             ["1000", 1, graph_date_timestamp, "1002"],
+#             ["1001", 2, graph_date_timestamp, "1000"],
+#             ["1001", 5, graph_date_timestamp, "1002"],
+#         ]
+#     )
 
-    # generating 3rd graph
-    int_matrix = {}
-    int_matrix[Activity.Reply] = np.array(
-        [
-            [0, 0, 3],
-            [0, 0, 0],
-            [1, 0, 0],
-        ]
-    )
-    int_matrix[Activity.Mention] = np.array(
-        [
-            [0, 0, 3],
-            [0, 0, 0],
-            [1, 0, 0],
-        ]
-    )
-    int_matrix[Activity.Reaction] = np.array(
-        [
-            [0, 0, 3],
-            [0, 0, 0],
-            [1, 0, 0],
-        ]
-    )
+#     # generating 3rd graph
+#     int_matrix = {}
+#     int_matrix[Activity.Reply] = np.array(
+#         [
+#             [0, 0, 3],
+#             [0, 0, 0],
+#             [1, 0, 0],
+#         ]
+#     )
+#     int_matrix[Activity.Mention] = np.array(
+#         [
+#             [0, 0, 3],
+#             [0, 0, 0],
+#             [1, 0, 0],
+#         ]
+#     )
+#     int_matrix[Activity.Reaction] = np.array(
+#         [
+#             [0, 0, 3],
+#             [0, 0, 0],
+#             [1, 0, 0],
+#         ]
+#     )
 
-    graph = generate_mock_graph(int_matrix, acc_names)
-    nx.set_node_attributes(graph, node_att, "acc_name")
+#     graph = generate_mock_graph(int_matrix, acc_names)
+#     nx.set_node_attributes(graph, node_att, "acc_name")
 
-    graph_date = datetime.now() + timedelta(days=-8)
-    graph_date_timestamp = graph_date.replace(
-        hour=0, minute=0, second=0, microsecond=0
-    ).timestamp()
-    graph_dict[graph_date] = graph
+#     graph_date = datetime.now() + timedelta(days=-8)
+#     graph_date_timestamp = graph_date.replace(
+#         hour=0, minute=0, second=0, microsecond=0
+#     ).timestamp()
+#     graph_dict[graph_date] = graph
 
-    desired_outputs.extend(
-        [
-            ["1000", 3, graph_date_timestamp, "1002"],
-            ["1002", 1, graph_date_timestamp, "1000"],
-        ]
-    )
+#     desired_outputs.extend(
+#         [
+#             ["1000", 3, graph_date_timestamp, "1002"],
+#             ["1002", 1, graph_date_timestamp, "1000"],
+#         ]
+#     )
 
     # DATABASE SAVING
 
